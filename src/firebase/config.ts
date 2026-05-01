0ce64aaa71b814ba479c8a2448a87b54412bcc21
Ujal Bag <ujalbag96@gmail.com>
2026-05-01 08:35:34 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1481658247-8fea7",
+  "appId": "1:517266169602:web:e6769a7e07c1f05aecac1b",
+  "apiKey": "AIzaSyCs3h3Jn1xkCyLDurWp1QFWf21_LkB-PBk",
+  "authDomain": "studio-1481658247-8fea7.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "517266169602"
+};
