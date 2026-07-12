5469eaef5a3f2b4ed85a570313340f71611601d4
Ujal Bag <ujalbag96@gmail.com>
2026-07-12 10:20:07 +0000

    const firebaseConfig = {   apiKey: "AIzaSyBct1Ggy4ZCVD1sX3t67B...",   au

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,10 @@
 export const firebaseConfig = {
   apiKey: "AIzaSyBbct10gy4ZCYDldsk3t678RWgJDEHX2zY",
   authDomain: "protournament7.firebaseapp.com",
+  databaseURL: "https://protournament7-default-rtdb.firebaseio.com",
   projectId: "protournament7",
   storageBucket: "protournament7.firebasestorage.app",
-  messagingSenderId: "647792725979",
-  appId: "1:647792725979:web:c3216157c073ae337a61fe",
-  measurementId: "G-T8LY2LXREB"
+  messagingSenderId: "647792735979",
+  appId: "1:647792735979:web:c3216157cd73ae237a61fe",
+  measurementId: "G-TRLY2EXDHR"
 };
