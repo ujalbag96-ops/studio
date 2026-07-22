fa1e90ade7fbdbb060b436bbdc7bf758164f8edc
Ujal Bag <ujalbag96@gmail.com>
2026-07-22 06:25:40 +0000

    import * as React from "react" import * as DialogPrimitive from "@radix-

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -2,6 +2,7 @@
 
 import * as React from "react"
 import * as DialogPrimitive from "@radix-ui/react-dialog"
+import * as VisuallyHidden from "@radix-ui/react-visually-hidden"
 import { X } from "lucide-react"
 
 import { cn } from "@/lib/utils"
@@ -31,8 +32,8 @@ DialogOverlay.displayName = DialogPrimitive.Overlay.displayName
 
 const DialogContent = React.forwardRef<
   React.ElementRef<typeof DialogPrimitive.Content>,
-  React.ComponentPropsWithoutRef<typeof DialogPrimitive.Content>
->(({ className, children, ...props }, ref) => (
+  React.ComponentPropsWithoutRef<typeof DialogPrimitive.Content> & { title?: string }
+>(({ className, children, title, ...props }, ref) => (
   <DialogPortal>
     <DialogOverlay />
     <DialogPrimitive.Content
@@ -43,6 +44,11 @@ const DialogContent = React.forwardRef<
       )}
       {...props}
     >
+      {title && (
+        <VisuallyHidden.Root>
+          <DialogPrimitive.Title>{title}</DialogPrimitive.Title>
+        </VisuallyHidden.Root>
+      )}
       {children}
       <DialogPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground">
         <X className="h-4 w-4" />
