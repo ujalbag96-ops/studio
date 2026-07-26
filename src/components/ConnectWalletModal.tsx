736ad082652404b91b2bac78acdbfa396c73da3d
Ujal Bag <ujalbag96@gmail.com>
2026-07-26 04:42:12 +0000

    npm install

--- src/components/ConnectWalletModal.tsx
+++ src/components/ConnectWalletModal.tsx
@@ -32,7 +32,7 @@ import { doc } from 'firebase/firestore';
 import { AppSettings, UserProfile } from '@/app/lib/types';
 import { useToast } from '@/hooks/use-toast';
 import { Input } from './ui/input';
-import { Label } from './ui/label';
+import { Label } from './label';
 import RiskDisclosureModal from './RiskDisclosureModal';
 
 interface ConnectWalletModalProps {
@@ -288,4 +288,30 @@ function MethodButton({ icon, label, active, onClick }: any) {
          <span className="text-[10px] font-black uppercase">{label}</span>
       </button>
    );
+}
+
+function Loader2(props: any) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+    >
+      <path d="M12 2v4" />
+      <path d="m16.2 7.8 2.9-2.9" />
+      <path d="M18 12h4" />
+      <path d="m16.2 16.2 2.9 2.9" />
+      <path d="M12 18v4" />
+      <path d="m4.9 19.1 2.9-2.9" />
+      <path d="M2 12h4" />
+      <path d="m4.9 4.9 2.9 2.9" />
+    </svg>
+  );
 }
